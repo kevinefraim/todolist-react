92d46be085b9fd07103da0ee145161d48774e80d
Kevin Efraim <kevinefraim02@gmail.com>
2022-01-03 17:59:58 -0300

    ejercicio finalizado

--- src/components/Lista/Lista.jsx
+++ src/components/Lista/Lista.jsx
@@ -1,19 +1,21 @@
-import DeleteIcon from "@mui/icons-material/Delete";
 import { IconButton, ListItem, ListItemText } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const Lista = (name, id) => {
+const Lista = ({ name, id, removerTarea }) => {
   return (
-    <>
-      <ListItem
-        secondaryAction={
-          <IconButton edge="end" aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        }
-      >
-        <ListItemText primary={name} />
-      </ListItem>
-    </>
+    <ListItem
+      secondaryAction={
+        <IconButton
+          edge="end"
+          aria-label="delete"
+          onClick={() => removerTarea(id)}
+        >
+          <DeleteIcon />
+        </IconButton>
+      }
+    >
+      <ListItemText primary={name} />
+    </ListItem>
   );
 };
 
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 import "./ToDoList.css";
-import {
-  Grid,
-  IconButton,
-  List,
-  ListItem,
-  TextField,
-  Typography,
-  Box,
-  ListItemText,
-} from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { Grid, List, TextField, Typography, Box } from "@mui/material";
 import { useState } from "react";
+import { grey } from "@mui/material/colors";
 import Lista from "../Lista/Lista";
 
 const ToDoList = () => {
@@ -26,6 +17,12 @@ const ToDoList = () => {
     if (tarea === "") return;
     //crear objeto con el ID y el nombre de la tarea
     setLista([{ id: new Date().getTime(), name: tarea }, ...lista]);
+    setTarea("");
+  };
+
+  //Funcion de remover tarea seleccionada
+  const removerTarea = (id) => {
+    setLista(lista.filter((listItem) => listItem.id != id));
   };
 
   return (
@@ -44,11 +41,17 @@ const ToDoList = () => {
       </form>
       <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
         <Grid item xs={12} md={6}>
-          <List>
-            {/* {lista.map((todo) => (
-              <Lista key={todo.id} {...todo} />
-            ))} */}
-          </List>
+          {lista.length === 0 ? (
+            <Typography variant="h4" align="center" color={grey[400]} mt={3}>
+              Agregue tareas aquí
+            </Typography>
+          ) : (
+            <List>
+              {lista.map((todo) => (
+                <Lista key={todo.id} {...todo} removerTarea={removerTarea} />
+              ))}
+            </List>
+          )}
         </Grid>
       </Box>
     </div>
